506184eacd7d47d5bd53ce2dc45e3db4c5e4405a
Luke <bassett.luke@gmail.com>
2020-08-26 22:10:03 -0700

    folder structure change

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # f1-analysis
 Formula 1 data collection, visulaization, and analysis.
+
+## To do:
+- full team colors data
+
--- f1-analysis/refresh_ergast.py
+++ f1-analysis/refresh_ergast.py
@@ -2,13 +2,13 @@ import os
 import urllib.request
 import zipfile
 
-def refresh(path='data/ergast_data', url='http://ergast.com/downloads/f1db_csv.zip'):
+def refresh(path='../data/ergast_data', url='http://ergast.com/downloads/f1db_csv.zip'):
     """download latest ergast data and replace data in datadir"""  
     if not os.path.exists(path):
         os.makedirs(path)
         print('created path', path)
     else:
-        print('foud path', path)
+        print('found path', path)
     zf_path = os.path.join(path, 'f1db_csv.zip')
     print('downloading ergast data')
     urllib.request.urlretrieve(url, zf_path)
