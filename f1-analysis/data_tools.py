1463049ccc4aa457bef1f03d395e792abed9fc82
luke_bassett <bassett.luke@gmail.com>
2020-08-30 16:37:42 -0700

    added driver_ref arg to make_delta_table()

--- f1-analysis/data_tools.py
+++ f1-analysis/data_tools.py
@@ -29,7 +29,14 @@ def tgt_driver_delta(grp, tgt):
     return grp
 
 
-def make_delta_table(ergast, raceId, tgt_driver, driver_ids=None, driver_codes=None):
+def get_driver_codes(ergast):
+    """return dict where key is driverId and val is code"""
+    return ergast.data["drivers"].set_index("driverId")[["code"]].to_dict()["code"]
+
+
+def make_delta_table(
+    ergast, raceId, tgt_driver, driver_ids=None, driver_refs=None, driver_codes=None
+):
     """tgt_driver must be reference(str) or id(int)"""
 
     if isinstance(tgt_driver, str):
@@ -38,25 +45,14 @@ def make_delta_table(ergast, raceId, tgt_driver, driver_ids=None, driver_codes=N
     laps = load_race_laps(ergast, raceId)
     laps = calc_total_milliseconds(laps)
 
-    res = load_race_results(ergast, raceId)
-
-    codes = ergast.data["drivers"].set_index("driverId")[["code"]].to_dict()["code"]
-    laps["driver_code"] = laps["driverId"].map(codes)
-
-    colors = {}
-    for driverId in res["driverId"].unique():
-        constructorId = res.loc[
-            res["driverId"] == driverId, "constructorId"
-        ].to_numpy()[0]
-        con = ergast.data["constructors"].copy()
-        constructor = con.loc[
-            con["constructorId"] == constructorId, "constructorRef"
-        ].to_numpy()[0]
-        colors[codes[driverId]] = style.team_colors[constructor]
-
     if driver_ids:
         laps = laps[laps["driverId"].isin(driver_ids)]
+    elif driver_refs:
+        laps["driverRef"] = laps["driverId"].map(get_driverid_to_driverref_dict(ergast))
+        laps = laps[laps["driverRef"].isin(driver_refs)]
     elif driver_codes:
+        codes = get_driver_codes(ergast)
+        laps["driver_code"] = laps["driverId"].map(codes)
         laps = laps[laps["driver_code"].isin(driver_codes)]
 
     # remove laps which targer driver did not complete
@@ -66,7 +62,7 @@ def make_delta_table(ergast, raceId, tgt_driver, driver_ids=None, driver_codes=N
     laps = laps.groupby("lap").apply(tgt_driver_delta, tgt_driver)
 
     laps["driverRef"] = laps["driverId"].map(
-        get_driverid_to_driverref_dict(ergast, list(laps["driverId"].unique()))
+        get_driverid_to_driverref_dict(ergast, laps["driverId"].unique())
     )
 
     return laps
@@ -96,46 +92,56 @@ def get_driver_color_dict(ergast, raceId, key="id"):
     return colors
 
 
-def get_driverid_to_code_dict(ergast, driver_ids=None):
+def get_driverid_to_driverref_dict(ergast, driver_ids="all"):
     """return a dict where key is driverId and val is code"""
     d = ergast.data["drivers"].copy()
-    if driver_ids:
-        d = d[d["driverId"].isin(driver_ids)]
-    d.set_index("driverId", inplace=True)
-    return d["code"].to_dict()
 
-
-def get_code_to_driverid_dict(ergast, codes=None):
-    """return a dict where key is code and val is driverId"""
-    d = ergast.data["drivers"].copy()
-    if codes:
-        d = d[d["code"].isin(codes)]
-    d = d[d["code"] != "\\N"]
-    d.set_index("code", inplace=True)
-
-    return d["driverId"].to_dict()
-
-
-def get_driverid_to_driverref_dict(ergast, driver_ids=None):
-    """return a dict where key is driverId and val is code"""
-    if not isinstance(driver_ids, list):
+    if str(driver_ids) != "all":
         driver_ids = list(driver_ids)
-    d = ergast.data["drivers"].copy()
-    if driver_ids:
         d = d[d["driverId"].isin(driver_ids)]
+
     d.set_index("driverId", inplace=True)
     return d["driverRef"].to_dict()
 
 
-def get_driverref_to_driverid_dict(ergast, driver_refs=None):
+def get_driverid_to_code_dict(ergast, driver_ids="all"):
     """return a dict where key is driverId and val is code"""
     d = ergast.data["drivers"].copy()
-    if driver_refs:
+
+    if str(driver_ids) != "all":
+        driver_ids = list(driver_ids)
+        d = d[d["driverId"].isin(driver_ids)]
+
+    d.set_index("driverId", inplace=True)
+    return d["code"].to_dict()
+
+
+def get_code_to_driverid_dict(ergast, codes="all"):
+    """return a dict where key is code and val is driverId"""
+    d = ergast.data["drivers"].copy()
+
+    if str(codes) != "all":
+        codes = list(codes)
+        d = d[d["code"].isin(codes)]
+
+    d = d[d["code"] != "\\N"]
+
+    d.set_index("code", inplace=True)
+    return d["driverId"].to_dict()
+
+
+def get_driverref_to_driverid_dict(ergast, driver_refs="all"):
+    """return a dict where key is driverId and val is code"""
+    d = ergast.data["drivers"].copy()
+
+    if str(driver_refs) != "all":
+        driver_refs = list(driver_refs)
         d = d[d["driverRef"].isin(driver_refs)]
+
     d.set_index("driverRef", inplace=True)
     return d["driverId"].to_dict()
 
 
-def driverid_to_driverref(driverIds):
-    d = get_driverid_to_driverref_dict(list(driverIds))
-    return [d[x] for x in driverIds]
+def driverid_to_driverref(driver_ids):
+    d = get_driverid_to_driverref_dict(list(driver_ids))
+    return [d[x] for x in driver_ids]
