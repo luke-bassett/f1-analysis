15afd766056138e7ab50c834ff627077383bd12c
luke_bassett <bassett.luke@gmail.com>
2020-09-01 16:39:27 -0700

    updated colors

--- f1-analysis/style.py
+++ f1-analysis/style.py
@@ -1,41 +1,44 @@
 """Style config used for plotting"""
 
+# Colors extracted from images on formula1.com.
 team_colors = {
-    "mercedes": "#00D2BE",
-    "ferrari": "#C00000",
-    "red_bull": "#0600EF",
+    "mercedes": "#03d1bf",
+    "ferrari": "#dc0000",
+    "red_bull": "#1b43ff",
     "renault": "#FFF500",
-    "haas": "#787878",
-    "racing_point": "#F596C8",
-    "alphatauri": "#C8C8C8",
-    "mclaren": "#FF8700",
-    "alfa": "#960000",
-    "williams": "#0082FA",
+    "haas": "#9c9c9c",
+    "racing_point": "#f496c8",
+    "alphatauri": "#15151d",
+    "mclaren": "#ff8700",
+    "alfa": "#9a0000",
+    "williams": "#459bfe",
 }
 
-
+# based on t-cam color; black is solid yellow is dashed
+solid = (1, 0)
+dashed = (3, 1)
 dashes_2020 = {
-    # cool formula 1 drivers
-    "hamilton": (1, 0),
-    "bottas": (3, 1),
-    "max_verstappen": (1, 0),
-    "albon": (3, 1),
-    "perez": (1, 0),
-    "stroll": (3, 1),
-    "ricciardo": (1, 0),
-    "ocon": (3, 1),
-    "gasly": (3, 1),
-    "kvyat": (1, 0),
-    "sainz": (1, 0),
-    "norris": (3, 1),
-    "leclerc": (3, 1),
-    "vettel": (1, 0),
-    "raikkonen": (1, 0),
-    "giovinazzi": (3, 1),
-    "russell": (1, 0),
-    "latifi": (3, 1),
-    "kevin_magnussen": (3, 1),
-    "hulkenberg": (1, 0),
+    # qualified formula 1 drivers
+    "hamilton": solid,
+    "bottas": dashed,
+    "max_verstappen": solid,
+    "albon": dashed,
+    "perez": solid,
+    "stroll": dashed,
+    "ricciardo": solid,
+    "ocon": dashed,
+    "gasly": dashed,
+    "kvyat": solid,
+    "sainz": solid,
+    "norris": dashed,
+    "leclerc": dashed,
+    "vettel": solid,
+    "raikkonen": solid,
+    "giovinazzi": dashed,
+    "russell": solid,
+    "latifi": dashed,
+    "kevin_magnussen": dashed,
+    "hulkenberg": solid,
     # other
-    "grosjean": (1, 0),
+    "grosjean": solid,
 }
