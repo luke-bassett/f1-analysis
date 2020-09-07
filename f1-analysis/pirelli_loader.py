203b8a8c04c766e7d8d4726eb5e431bdc06a6f7f
luke_bassett <bassett.luke@gmail.com>
2020-09-07 15:35:23 -0700

    simplified pirelli loader

--- f1-analysis/pirelli_loader.py
+++ f1-analysis/pirelli_loader.py
@@ -1,27 +1,11 @@
 """Pirelli data holder"""
 import os
-import re
 import pandas as pd
 
 
-class PirelliLoader:
-    """Creates a data object to hold pirelli tyre data"""
-
-    def __init__(self, path):
-        """Loads pirelli data to dataframes.
-
-        path (str):
-            Location in which data is stored. Must be stored as csvs.
-
-        """
-
-        self.path = path
-        self.data = {}
-        self.load()
-
-    def load(self):
-        """Loads each csv listed in tables as a dataframe in the data dict"""
-        pat = r"[\D]*(\d{4})[\D]*"
-        for f in os.listdir(self.path):
-            year = int(re.findall(pat, f)[0])
-            self.data[year] = pd.read_csv(os.path.join(self.path, f))
+def load_pirelli_tyre_data(path='data/pirelli'):
+    """Return df containing all data in data/pirelli"""
+    dfs = []
+    for f in os.listdir(path):
+        dfs.append(pd.read_csv(os.path.join(path, f)))
+    return pd.concat(dfs)
